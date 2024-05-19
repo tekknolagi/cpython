51d6e62f1b8dd920613d4980d2a18f1af845dfc8
Max Bernstein <max@bernsteinbear.com>
2024-05-19 15:31:57 -0400

    Add helper scripts

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+export PATH="/home/max/Documents/dev/wasmtime/target/release/:$PATH"
+
+set -eux
+python3.12 Tools/wasm/wasm_build.py wasi build > /dev/null
+
+PYTHONPATH=$(pwd)/builddir/wasi/build/lib.wasi-wasm32-3.14:$(pwd)/Lib \
+wizer -r _start=wizer_resume \
+  --dir . \
+  --inherit-env true \
+  --allow-wasi \
+  --wasm-bulk-memory true \
+  -o builddir/wasi/python.wizer.wasm \
+  builddir/wasi/python.wasm \
+  > /dev/null

7015b21d49db4e73c0413f4ada31daad4b48a7c7
Max Bernstein <max@bernsteinbear.com>
2024-06-13 15:08:51 -0400

    Remove unused preview2

--- run.sh
+++ run.sh
@@ -2,7 +2,6 @@
 export PATH="/home/max/Documents/dev/wasmtime/target/release/:$PATH"
 set -eux
 wasmtime run --wasm max-wasm-stack=8388608 \
-  --wasi preview2 \
   --dir /home/max/Documents/dev/cpython::/ \
   --dir /home/max/Documents/dev/cpython/builddir/wasi/build/lib.wasi-wasm32-3.14 \
   --env PYTHONPATH=/builddir/wasi/build/lib.wasi-wasm32-3.14:/Lib \
